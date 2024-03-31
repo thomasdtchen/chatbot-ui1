d691551ee6b325bfe3f60bf59e0397a64df4d28d
thomas <952711872@qq.com>
2024-03-31 20:51:18 +0800

    1st work version

---
+++ types/openai.ts
@@ -0,0 +1,29 @@
+export interface OpenAIModel {
+  id: string;
+  name: string;
+  maxLength: number; // maximum length of a message
+  tokenLimit: number;
+}
+
+export enum OpenAIModelID {
+  GPT_3_5 = 'gpt-3.5-turbo',
+  gpt_4_turbo_preview = 'gpt-4-turbo-preview',
+}
+
+// in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
+export const fallbackModelID = OpenAIModelID.GPT_3_5;
+
+export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
+  [OpenAIModelID.GPT_3_5]: {
+    id: OpenAIModelID.GPT_3_5,
+    name: 'GPT-3.5',
+    maxLength: 12000,
+    tokenLimit: 4000,
+  },
+  [OpenAIModelID.gpt_4_turbo_preview]: {
+    id: OpenAIModelID.gpt_4_turbo_preview,
+    name: 'gpt-4-turbo-preview',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },
+};
